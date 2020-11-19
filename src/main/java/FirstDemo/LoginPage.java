72b6a889e1771beccced74bdee2d0d9ce74fb8bd
ShanmugaSrinivasSunnapu <shanm@DESKTOP-RIL1GV>
2020-11-19 19:08:46 +0530

    19-11-2020 Commit

--- src/main/java/FirstDemo/LoginPage.java
+++ src/main/java/FirstDemo/LoginPage.java
@@ -1,6 +1,9 @@
 package FirstDemo;
 
+import org.testng.annotations.Test;
+
 public class LoginPage { 
+	@Test
 	public void loginPage() {
 		System.out.println("loginPage");
 		System.out.println("This is first comment ");
